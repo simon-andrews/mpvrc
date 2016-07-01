2f6e8b10c57faddcfe7839f3d4d28df50fbf1602
Simon Andrews <simon.andrews212@gmail.com>
2016-06-30 20:28:58 -0400

    Glue everything together

--- src/main/java/org/simonandrews/mpvrc/ControlServer.java
+++ src/main/java/org/simonandrews/mpvrc/ControlServer.java
@@ -1,14 +1,22 @@
 package org.simonandrews.mpvrc;
 
+import java.awt.*;
+
 import static spark.Spark.get;
 
 public class ControlServer {
-    public static void main(String[] args) {
-        get(UrlMap.INDEX, (req, res) -> "hello world");
-        get(UrlMap.SEEK_FORWARD_5, (req, res) -> "hello world");
-        get(UrlMap.SEEK_BACKWARD_5, (req, res) -> "hello world");
-        get(UrlMap.SEEK_FORWARD_60, (req, res) -> "hello world");
-        get(UrlMap.SEEK_BACKWARD_60, (req, res) -> "hello world");
-        get(UrlMap.TOGGLE_PLAYBACK, (req, res) -> "hello world");
+    public static String SUCCESS_MESSAGE = "OK";
+    public static String FAILURE_MESSAGE = "NOT OK";
+    public static void main(String[] args) throws AWTException {
+        System.out.println("ayy");
+        MpvController controller = new MpvController();
+        System.out.println("ayy");
+        get(UrlMap.INDEX, (req, res) -> FAILURE_MESSAGE);
+        get(UrlMap.SEEK_FORWARD_5, (req, res) -> controller.seekForward5());
+        get(UrlMap.SEEK_BACKWARD_5, (req, res) -> controller.seekBackward5());
+        get(UrlMap.SEEK_FORWARD_60, (req, res) -> controller.seekForward60());
+        get(UrlMap.SEEK_BACKWARD_60, (req, res) -> controller.seekBackward60());
+        get(UrlMap.TOGGLE_PLAYBACK, (req, res) -> controller.togglePlayback());
+        System.out.println("ayy");
     }
 }
--- src/main/java/org/simonandrews/mpvrc/MpvController.java
+++ src/main/java/org/simonandrews/mpvrc/MpvController.java
@@ -1,23 +1,35 @@
 package org.simonandrews.mpvrc;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class MpvController {
-    public static void seekForward5() {
-
+    private Robot robot;
+    public MpvController() throws AWTException{
+        robot = new Robot();
     }
-
-    public static void seekBackward5() {
-
+    public String seekForward5() {
+        pressAndRelease(KeyEvent.VK_LEFT);
+        return ControlServer.SUCCESS_MESSAGE;
     }
-
-    public static void seekForward60() {
-
+    public String seekBackward5() {
+        pressAndRelease(KeyEvent.VK_RIGHT);
+        return ControlServer.SUCCESS_MESSAGE;
     }
-
-    public static void seekBackward60() {
-
+    public String seekForward60() {
+        pressAndRelease(KeyEvent.VK_UP);
+        return ControlServer.SUCCESS_MESSAGE;
     }
-
-    public static void togglePlayback() {
-
+    public String seekBackward60() {
+        pressAndRelease(KeyEvent.VK_DOWN);
+        return ControlServer.SUCCESS_MESSAGE;
+    }
+    public String togglePlayback() {
+        pressAndRelease(KeyEvent.VK_SPACE);
+        return ControlServer.SUCCESS_MESSAGE;
+    }
+    private void pressAndRelease(int keycode) {
+        //robot.keyPress(keycode);
+        //robot.keyRelease(keycode);
     }
 }
